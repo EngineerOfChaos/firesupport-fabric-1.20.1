edab600b930cb056096e7ebf584f9909df24dccc
engineerofchaos <adam@romano-critchley.com>
2025-05-31 19:09:12 +0100

    Initial commit

---
+++ src/main/java/net/engineerofchaos/firesupport/FireSupportClient.java
@@ -0,0 +1,10 @@
+package net.engineerofchaos.firesupport;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FireSupportClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
